1e2f5279589c612c31c3c479ea825eb375e6b8b0
Yakymchuk Artur <Yakymchuk.Artur@formpipe.com>
2023-06-15 22:02:29 +0300

    add methods

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TelegrafModule } from 'nestjs-telegraf';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TelegrafModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        token: configService.get<string>('BOT_TOKEN'),
+        launchOptions: {
+          webhook: {
+            domain: configService.get<string>('VERCEL_URL'),
+            hookPath: '/secret-path',
+          }
+        }
+      }),
+      inject: [ConfigService],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
