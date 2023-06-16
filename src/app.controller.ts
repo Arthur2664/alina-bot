f616b6a0424498471f4c918e2d683330e979b8ca
Yakymchuk Artur <Yakymchuk.Artur@formpipe.com>
2023-06-16 23:59:00 +0300

    CRON

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,36 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { createKysely } from '@vercel/postgres-kysely';
+import { Input, Telegram } from 'telegraf';
+import { Readable } from 'stream';
+
+
+interface ImageTable {
+  data: Uint8Array;
+}
+
+interface Database {
+  image: ImageTable;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  private readonly bot: Telegram = new Telegram(process.env.BOT_TOKEN);
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<string> {
+    const db = createKysely<Database>();
+    const data = await db
+      .selectFrom('image')
+      .select('data')
+      .executeTakeFirst();
+
+    const stream = Readable.from(data.data);
+
+    const file = Input.fromReadableStream(stream);
+    await this.bot.sendPhoto('-1001739837583', file);
+    return "SENT!!!"
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,34 +4,10 @@ import { createKysely } from '@vercel/postgres-kysely';
 import { Readable } from 'stream';
 import { Input, Telegram } from 'telegraf';
 
-interface ImageTable {
-  data: Uint8Array;
-}
-
-interface Database {
-  image: ImageTable;
-}
-
 @Injectable()
 export class AppService {
 
-  private readonly bot: Telegram = new Telegram(process.env.BOT_TOKEN);
-
   getHello(): string {
     return 'Hello World!';
-  }
-
-  @Cron(`47 23 * * *`, {timeZone: 'Europe/Kiev'})
-  async runCronEvery30Seconds() {
-    const db = createKysely<Database>();
-    const data = await db
-      .selectFrom('image')
-      .select('data')
-      .executeTakeFirst();
-
-    const stream = Readable.from(data.data);
-
-    const file = Input.fromReadableStream(stream);
-    await this.bot.sendPhoto('-1001739837583', file);
   }
 }
