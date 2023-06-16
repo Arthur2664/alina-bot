e9d5fb56834e4bdfda3f3de554ea5683b2a4db6e
Yakymchuk Artur <Yakymchuk.Artur@formpipe.com>
2023-06-16 19:10:28 +0300

    wtf

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const bot = app.get(getBotToken());
 
-  const botInfo = await bot.telegram.getMe();
-	bot.options.username = botInfo.username;
-	console.info("Server has initialized bot username using Webhook. ", botInfo.username);
+  // const botInfo = await bot.telegram.getMe();
+	// bot.options.username = botInfo.username;
+	// console.info("Server has initialized bot username using Webhook. ", botInfo.username);
 
-  
+
   app.use(bot.webhookCallback('/secret-path'));
   await app.listen(3000);
 }
